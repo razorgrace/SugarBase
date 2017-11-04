3429f5521f7ff686575e892b04161ef3640d9e36
razorgrace <33260421+razorgrace@users.noreply.github.com>
2017-11-05 00:00:04 +0200

    added mount meta object

--- SugarBase.lua
+++ SugarBase.lua
@@ -293,3 +293,89 @@ lib.UpdateData = function () _G[savedVariable] = updateDatabase(_G[savedVariable
 lib.GetData = function () return addonNamespace.Data end
 lib.GetSpellMetaObject = function () return addonNamespace.SpellMetaObjectFactory(addonNamespace.Data) end
 lib.GetPlayerMetaObject = function () return addonNamespace.PlayerMetaObjectFactory(addonNamespace.Data) end
+
+lib.GetMountMetaObject = (function ()
+	-- LoadAddOn("Blizzard_MountCollection")
+        
+	local getMountInfoById = function (mountID)
+        local creatureName, spellID, icon, active, isUsable, sourceType, isFavorite, isFactionSpecific, faction, hideOnChar, isCollected, mountID = C_MountJournal.GetMountInfoByID(mountID)
+
+        if not creatureName then
+            return nil
+        end
+
+        return {
+            creatureName = creatureName,
+            spellID = spellID,
+            icon = icon,
+            active = active,
+            isUsable = isUsable,
+            sourceType = sourceType,
+            isFavorite = isFavorite,
+            isFactionSpecific = isFactionSpecific,
+            faction = faction,
+            hideOnChar = hideOnChar,
+            isCollected = isCollected,
+            mountID = mountID,
+        }
+    end
+
+    local mountInfoById = setmetatable({}, { __index = function (self, index)
+        local record = getMountInfoById(index)
+
+        if not record then
+            return nil
+        end
+
+        self[index] = record
+
+        return record
+    end })
+
+    local function findMount(predicate)
+        local incomplete = nil
+
+        for mountID = 1, C_MountJournal.GetNumMounts() do
+            local record = mountInfoById[mountID]
+
+            if not record then
+                incomplete = true
+            elseif predicate(record) then
+                return record, incomplete
+            end
+        end
+
+        if incomplete == nil then
+            incomplete = false
+        end
+
+        return nil, incomplete
+    end
+
+    return function ()
+        return setmetatable({}, { __index = function (self, index)
+            local cachedRecord = findMount(function (record)
+                return addonNamespace.toCamelCase(record.creatureName) == index
+            end)
+            
+            if not cachedRecord then
+                return {}
+            end
+
+            local result = {
+                id = cachedRecord.mountID,
+                name = cachedRecord.creatureName,
+            }
+
+            local freshRecord = getMountInfoById(cachedRecord.mountID)
+
+            if freshRecord then
+                result.isUsable = freshRecord.isUsable
+            end
+
+            self[index] = result
+
+            return result
+        end } )
+    end
+end)()
