0577ee2a2b49f0045fad301207aed782b0fe9eca
razorgrace <33260421+razorgrace@users.noreply.github.com>
2017-11-04 09:24:49 +0200

    first implementation

---
+++ MetaObjects/Player.lua
@@ -0,0 +1,99 @@
+local addonName, addonNamespace = ...
+
+addonNamespace.PlayerMetaObjectFactory = function (data)
+    local result = {}
+
+    local function static(index, records, indexer, predicate)
+        local subset = {}
+
+        for _, record in pairs(records) do
+            if indexer(record) == index and predicate(record) then
+                return function () return true end
+            end
+        end
+    end
+
+    local function dynamic(index, records, indexer, predicate)
+        local subset = {}
+
+        for _, record in pairs(records) do
+            if indexer(record) == index then
+                table.insert(subset, record)
+            end
+        end
+
+        if next(subset) then
+            return function ()
+                for _, record in pairs(subset) do
+                    if predicate(record) then
+                        return true
+                    end
+                end
+
+                return false
+            end
+        end
+    end
+
+    local getters = setmetatable({}, {
+        __index = function (self, index)
+            self[index] =
+                static(
+                    index,
+                    data.races,
+                    function (race) return 'is' .. addonNamespace.toCamelCase(race.name) end,
+                    function (race) return (select(2, UnitRace('player'))) == race.tag end
+                )
+                or static(
+                    index,
+                    data.classes,
+                    function (class) return 'is' .. addonNamespace.toCamelCase(class.name) end,
+                    function (class) return (select(3, UnitClass('player'))) == class.id end
+                )
+                or dynamic(
+                    index,
+                    data.roles,
+                    function (role) return 'is' .. addonNamespace.toCamelCase(role.name) end,
+                    function (role) return role.tag == (select(5, GetSpecializationInfo(GetSpecialization()))) end
+                )
+                or dynamic(
+                    index,
+                    data.specializations,
+                    function (specialization) return 'is' .. addonNamespace.toCamelCase(specialization.name) end,
+                    function (specialization) return specialization.id == (select(1, GetSpecializationInfo(GetSpecialization()))) end
+                )
+                or dynamic(
+                    index,
+                    data.specializations,
+                    function (specialization) return 'is' .. addonNamespace.toCamelCase(specialization.name) .. addonNamespace.toCamelCase(data.classes[specialization.classId].name) end,
+                    function (specialization) return specialization.id == (select(1, GetSpecializationInfo(GetSpecialization()))) end
+                )
+                or dynamic(
+                    index,
+                    data.talents,
+                    function (talent) return 'uses' .. addonNamespace.toCamelCase(talent.name) end,
+                    function (talent) return (select(4, GetTalentInfo(talent.tier, talent.column, 1))) end
+                )
+                or dynamic(
+                    index,
+                    data.talents,
+                    function (talent) return 'has' .. addonNamespace.toCamelCase(talent.name) end,
+                    function (talent) return (select(5, GetTalentInfo(talent.tier, talent.column, 1))) end
+                )
+                or dynamic(
+                    index,
+                    data.primaryStats,
+                    function (primaryStat) return 'uses' .. addonNamespace.toCamelCase(primaryStat.name) end,
+                    function (primaryStat) return (select(6, GetSpecializationInfo(GetSpecialization()))) == primaryStat.id end
+                )
+                or function () return nil end
+            return self[index]
+        end
+    })
+
+    return setmetatable(result, {        
+        __index = function (self, index)
+            return getters[index]()
+        end
+    })
+end
---
+++ SugarBase.lua
@@ -0,0 +1,295 @@
+local addonName, addonNamespace = ...
+
+LoadAddOn('LibStub')
+
+if not LibStub then
+    error('"LibStub" not found')
+    return
+end
+
+local lib = LibStub:NewLibrary(addonName .. '-' .. GetAddOnMetadata(addonName, 'Version'), 1)
+
+if not lib then
+    return
+end
+
+addonNamespace.toCamelCase = function (str)
+    return str:gsub('[^%w%s]', ''):gsub('(%w)(%w*)', function (a, b) return a:upper() .. b end):gsub('%s+', '')
+end
+
+local function updateDatabase(current)
+	local find = function (records, property, value)
+		for i, record in ipairs(records) do
+			if record[property] == value then
+				return record
+			end
+		end
+	end
+
+	local races = {
+		{
+			tag = 'Dwarf',
+			name = 'Dwarf',
+		},
+		{
+			tag = 'Draenei',
+			name = 'Draenei',
+		},
+		{
+			tag = 'Gnome',
+			name = 'Gnome',
+		},
+		{
+			tag = 'Human',
+			name = 'Human',
+		},
+		{
+			tag = 'NightElf',
+			name = 'Night Elf',
+		},
+		{
+			tag = 'Worgen',
+			name = 'Worgen',
+		},
+		{
+			tag = 'BloodElf',
+			name = 'Blood Elf',
+		},
+		{
+			tag = 'Goblin',
+			name = 'Goblin',
+		},
+		{
+			tag = 'Orc',
+			name = 'Orc',
+		},
+		{
+			tag = 'Tauren',
+			name = 'Tauren',
+		},
+		{
+			tag = 'Troll',
+			name = 'Troll',
+		},
+		{
+			tag = 'Scourge',
+			name = 'Scourge',
+		},
+		{
+			tag = 'Pandaren',
+			name = 'Pandaren',
+		},
+	}
+
+	local roles = {
+		[1] = {
+			id = 1,
+			tag = "DAMAGER",
+			name = "Damager",
+		},
+		[2] = {
+			id = 2,
+			tag = "TANK",
+			name = "Tank",
+		},
+		[3] = {
+			id = 3,
+			tag = "HEALER",
+			name = "Healer",
+		},
+	}
+
+	local classes = (function ()
+		local result = {}
+
+		for i = 1, GetNumClasses() do
+			local name, tag, id = GetClassInfo(i)
+			result[id] = {
+				id = id,
+				tag = tag,
+				name = name,
+			}
+		end
+
+		return result
+	end)()
+
+	local primaryStats = {
+		LE_UNIT_STAT_STRENGTH = {
+			id = LE_UNIT_STAT_STRENGTH,
+			name = 'Strength',
+		},
+		LE_UNIT_STAT_AGILITY = {
+			id = LE_UNIT_STAT_AGILITY,
+			name = 'Agility',
+		},
+		LE_UNIT_STAT_STAMINA = {
+			id = LE_UNIT_STAT_STAMINA,
+			name = 'Stamina',
+		},
+		LE_UNIT_STAT_INTELLECT = {
+			id = LE_UNIT_STAT_INTELLECT,
+			name = 'Intellect',
+		},
+	}
+
+	local specializations = (function ()
+		local result = {}
+
+		for i = 1, GetNumClasses() do
+			local _, _, classId = GetClassInfo(i)
+
+			for specializationIndex = 1, GetNumSpecializationsForClassID(classId) do
+				local id, name, _, iconId, roleTag, _, _ = GetSpecializationInfoForClassID(classId, specializationIndex)
+				result[id] = {
+					id = id,
+					index = specializationIndex,
+					classId = classId,
+					roleId = find(roles, 'tag', roleTag).id,
+					name = name,
+					icon = iconId,
+				}
+			end        
+		end
+
+		return result
+	end)()
+
+	local talents, spells = (function (current, spells)
+		local _, classTag, classId = UnitClass('player')
+
+		for specializationIndex = 1, GetNumSpecializationsForClassID(classId) do
+			local specializationId, _, _, _, _, _ = GetSpecializationInfoForClassID(classId, specializationIndex)
+			for tier, level in ipairs(CLASS_TALENT_LEVELS[classTag] or CLASS_TALENT_LEVELS.DEFAULT) do
+				for column = 1, 3 do
+                    local id, name, texture, _, _, spellId, _, _, _, _, _ = GetTalentInfoBySpecialization(specializationIndex, tier, column)
+                    print('GetTalentInfoBySpecialization', specializationIndex, tier, column, GetTalentInfoBySpecialization(specializationIndex, tier, column))
+					current[id] = {
+						id = id,
+						specializationId = specializationId,
+						name = name,
+						texture = texture,
+						level = level,
+						tier = tier,
+						column = column,
+						spellId = spellId,
+					}
+
+					local spellName, _, spellIcon, _, _, _, _ = GetSpellInfo(spellId)
+
+					spells[spellId] = {
+						id = spellId,
+						classId = classId,
+						name = spellName,
+						icon = spellIcon,
+						classId = classId,
+					}		
+				end
+			end
+		end
+					
+		return current, spells
+	end)(current.talents or {}, current.spells or {})
+
+	spells = (function (current)
+		local _, _, classId = UnitClass('player')
+		
+		local function forEachSpellBookItem(callback)
+			for tabIndex = 1, GetNumSpellTabs() do
+				local name, texture, offset, numberOfSpells = GetSpellTabInfo(tabIndex)
+				for spellIndex = 1, numberOfSpells do
+					if not IsPassiveSpell(spellIndex + offset, BOOKTYPE_SPELL) then
+						local itemType, itemId = GetSpellBookItemInfo(spellIndex + offset, BOOKTYPE_SPELL)
+	
+						if itemType == 'FLYOUT' then
+							local _, _, numberOfSlots, _ = GetFlyoutInfo(itemId)
+							for slotIndex = 1, numberOfSlots do
+								local spellId, _ = GetFlyoutSlotInfo(itemId, slotIndex)
+								local name, _, icon, _, _, _, _ = GetSpellInfo(spellId)
+								callback(spellId, name, icon, skillType, classId)
+							end
+						elseif itemType == 'SPELL' then
+							local name, _, icon, _, _, _, spellId = GetSpellInfo(itemId)
+							callback(spellId, name, icon, skillType, classId)
+						end
+					end
+				end
+			end
+		end
+
+		forEachSpellBookItem(function (spellId, name, icon, skillType, classId)
+			current[spellId] = {
+				id = spellId,
+				classId = classId,
+				name = name,
+				icon = icon,
+				classId = classId,
+			}
+		end)
+		
+		-- local seen = {}
+		-- local _, _, classId = UnitClass('player')
+		
+		-- for specializationIndex = 1, GetNumSpecializationsForClassID(classId) do
+		-- 	local specializationId, _, _, _, _, _ = GetSpecializationInfo(specializationIndex)
+		-- 	local spellIdsAndLevels = { GetSpecializationSpells(specializationIndex) }
+
+		-- 	for i = 1, #spellIdsAndLevels, 2 do
+		-- 		if spellIdsAndLevels[i] == 2006 then
+		-- 			print(spellIdsAndLevels[i])
+		-- 		end
+		-- 		local level = spellIdsAndLevels[i + 1]
+		-- 		local name, _, icon, _, _, _, spellId = GetSpellInfo(spellIdsAndLevels[i])
+
+		-- 		if name:find('Resu') then
+		-- 			-- print(name, icon, spellId)
+		-- 		end
+				
+		-- 		-- some spells (e.g. Exhilaration) have several IDs; going to use one that is resolved by game via name
+		-- 		local altName, _, altIcon, _, _, _, altSpellId = GetSpellInfo(name)
+
+		-- 		if altSpellId then
+		-- 			name = altName
+		-- 			icon = altIcon
+		-- 			spellId = altSpellId
+		-- 		end
+				
+		-- 		if not seen[spellId] then
+		-- 			seen[spellId] = true
+
+		-- 			current[spellId] = {
+		-- 				id = spellId,
+		-- 				classId = classId,
+		-- 				name = name,
+		-- 				icon = icon,
+		-- 				specializations = {},
+		-- 			}
+		-- 		end
+
+		-- 		current[spellId].specializations[specializationId] = {
+		-- 			id = specializationId,
+		-- 			level = level,
+		-- 		}
+		-- 	end
+		-- end
+
+		return current, spells
+	end)(spells)
+
+	current.races = races
+	current.roles = roles
+	current.primaryStats = primaryStats
+	current.classes = classes
+	current.specializations = specializations
+	current.talents = talents
+	current.spells = spells
+
+	return current
+end
+
+local savedVariable = GetAddOnMetadata(addonName, 'X-SavedVariable')
+
+lib.UpdateData = function () _G[savedVariable] = updateDatabase(_G[savedVariable] or addonNamespace.Data) end
+lib.GetData = function () return addonNamespace.Data end
+lib.GetSpellMetaObject = function () return addonNamespace.SpellMetaObjectFactory(addonNamespace.Data) end
+lib.GetPlayerMetaObject = function () return addonNamespace.PlayerMetaObjectFactory(addonNamespace.Data) end
